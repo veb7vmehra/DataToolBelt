4069544b049bd680cc19c76027529172a674afbc
Prayas Jain <prayas.jn24@gmail.com>
2020-09-29 14:43:14 +0530

    tried adding css to filedata(?)

--- back.py
+++ back.py
@@ -92,5 +92,5 @@ def stats():
         return render_template("filedata.html", filename = filename, feature = feature, func = func, ans = ans)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
